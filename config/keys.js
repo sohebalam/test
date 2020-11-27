f9a6fe1c979e836dcf152c688aa6c73cb63b0c69
soheb alam <sohebalam@hotmail.com>
2020-11-27 13:35:13 +0000

    one

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI: "mongodb://localhost:27017/test",
+  googleClientID:
+    "130210432692-8dujh2mskmmpa0ij50imvvmuv0e7b322.apps.googleusercontent.com",
+  googleClientSecret: "wyg0ZiGagZJ0Jm2u-Z556K8E",
+}
